28469e119ffc0109fc87517aa90112da5fa1c2c9
Leon Bai <leonbai0218@gmail.com>
2025-11-27 14:12:01 +0100

    Add DFA class for deterministic finite automaton implementation

---
+++ shield_synthesis/automaton/dfa.py
@@ -0,0 +1,33 @@
+State = int
+Label = int
+Action = int
+
+
+class DFA:
+    def __init__(
+        self,
+        states: list[State],
+        initial: State,
+        safe_states: list[State],
+        alphabet: list[tuple[Label, Action]],
+    ):
+        self.states = states
+        self.initial = initial
+        self.safe_states = set(safe_states)
+        self.alphabet = alphabet
+        self.transitions: dict[State, dict[tuple[Label, Action], State]] = {
+            q: {} for q in states
+        }
+
+    def add_transition(self, src: State, letter: tuple[Label, Action], dst: State):
+        assert src in self.states
+        assert dst in self.states
+        assert letter in self.alphabet
+
+        self.transitions[src][letter] = dst
+
+    def next_state(self, state: State, letter: tuple[Label, Action]) -> State:
+        return self.transitions[state][letter]
+
+    def is_safe_state(self, state: State) -> bool:
+        return state in self.safe_states
